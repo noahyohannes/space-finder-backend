0c1d8ea2bce859630cb21ec7c2a37eb0e7a1c2b8
noahyohannes <yohannes.no@gmail.com>
2022-03-23 16:19:03 -0400

    Linked lambda with api gateway

--- infrastructure/SpaceStack.ts
+++ infrastructure/SpaceStack.ts
@@ -2,8 +2,13 @@ import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { Code, Function as LambdaFunction, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { join } from 'path';
+import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway'
 
 export class SpaceStack extends Stack{
+
+
+    private api = new RestApi(this,'SpaceApi');
+
 
     constructor(scope: Construct, id: string, props: StackProps) {
         super(scope, id, props)
@@ -14,6 +19,13 @@ export class SpaceStack extends Stack{
             code: Code.fromAsset(join(__dirname, '..', '/services', 'hello')),
             handler: 'hello.main'
         })
+
+
+        //Hello Api Lambda Integration
+        const helloLambdaIntegration = new LambdaIntegration(helloLambda) 
+        const helloLambdaResource = this.api.root.addResource('hello');
+        helloLambdaResource.addMethod('GET', helloLambdaIntegration);
+
     }
         
 }
